b6502b23831115c244b20c322d320d9d6bc1dc91
Sebastián Brand <imsebastianbt@outlook.com>
2024-10-07 14:13:06 -0500

    Add countries API controller

---
+++ Fantasy/Fantasy.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,66 @@
+﻿using Fantasy.Backend.Data;
+using Fantasy.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountriesController : ControllerBase
+{
+	private readonly DataContext _context;
+
+	public CountriesController(DataContext context)
+	{
+		_context = context;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetAsync()
+	{
+		return Ok(await _context.Countries.ToListAsync());
+	}
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetAsync(int id)
+	{
+		var country = await _context.Countries.FindAsync(id);
+		if (country == null)
+			return NotFound();
+		return Ok(country);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> PostAsync(Country country)
+	{
+		_context.Countries.Add(country);
+		await _context.SaveChangesAsync();
+		return Ok(country);
+	}
+
+	[HttpPut]
+	public async Task<IActionResult> PutAsync(Country country)
+	{
+		var currentCountry = await _context.Countries.FindAsync(country.Id);
+		if (currentCountry == null)
+			return NotFound();
+
+		currentCountry.Name = country.Name;
+		_context.Countries.Update(currentCountry);
+		await _context.SaveChangesAsync();
+		return NoContent();
+	}
+
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteAsync(int id)
+	{
+		var country = await _context.Countries.FindAsync(id);
+		if (country == null)
+			return NotFound();
+
+		_context.Countries.Remove(country);
+		await _context.SaveChangesAsync();
+		return NoContent();
+	}
+}
